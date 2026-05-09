9d49a45754378e5f13cedf73fadcac515219aa2f
veerasaravanan88 <ramramghtm@gmail.com>
2026-05-09 10:31:44 +0530

    alpha exam sum 2

---
+++ alpha2.java
@@ -0,0 +1,11 @@
+class alpha2
+{
+	public static void main(String[]args)
+	{
+		char arr[]={'M','R','I','V','s'};
+		for(int i=arr.length-1;i>=0;i--)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
